9f28d8b3ddb7b7ec4a02333b2c3df54366f7067a
Shreyas24150 <shreyasacharya6516@gmail.com>
2025-08-04 18:23:34 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Hosting a Static Website on AWS S3 using Terraform
+hosted a real HTML website fully using Terraform
+```
+Cloud Infra (S3 + IAM)
+Frontend Website (HTML)
+Terraform (Infra-as-Code)
+```
+
+# Project structure
+```
+s3-static-site/
+├── website/          
+│   ├── index.html
+│   ├── styles.css
+│   ├── app.js
+│   └── logo.png
+├── main.tf           
+├── variables.tf
+└── outputs.tf
+```
+
+# 1. Setting up all the files
+
+# 2. Initialize terraform
+```
+terraform init
+```
+
+# 3. Apply Terraform
+```
+terraform apply
+```
+
+# 4. After successful terraform apply upload website files to S3
+```
+aws s3 sync website/ s3://s3-web-tf-bug24-20250804
+```
+
+# 5. Open Your Website
+After terraform apply, you'll see an output like
+```
+website_url = http://s3-web-tf-bug24-20250804.s3-website-us-east-1.amazonaws.com
+```
+
+**Open this URL in your browser!**
+
+
+**Note**
+**Destroying terraform**
+
+Manually Empty the Bucket Before Deletion
+```
+aws s3 rm s3://s3-web-tf-bug24-20250804 --recursive
+```
---
+++ main.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_s3_bucket" "static_site" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "MyStaticWebsite"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "site_config" {
+  bucket = aws_s3_bucket.static_site.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.static_site.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "allow_public" {
+  bucket = aws_s3_bucket.static_site.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
